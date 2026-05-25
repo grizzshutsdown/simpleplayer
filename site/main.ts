809619e45edba75ce83ced38b2c26c2c30aade16
GrizzShutsDown <grizzshutsdown@gmail.com>
2026-05-25 10:33:20 +0100

    Show installed package version on site

--- site/main.ts
+++ site/main.ts
@@ -1,1 +1,8 @@
 import '@grizzshutsdown/simpleplayer';
+import simplePlayerPackage from './node_modules/@grizzshutsdown/simpleplayer/package.json';
+
+const version = document.querySelector('[data-simpleplayer-version]');
+
+if (version) {
+  version.textContent = `v${simplePlayerPackage.version}`;
+}
